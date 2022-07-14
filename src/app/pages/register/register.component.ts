1d2c08d89dca2672f3625083d559ed73a135d495
Alejandro Guerrero <alejo.jesus.magne@gmail.com>
2022-07-13 20:41:10 -0400

    login and register

--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent implements OnInit {
-  hide = true;
+  hide: boolean = true;
   constructor() { }
 
   ngOnInit(): void {
